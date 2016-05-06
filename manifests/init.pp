5b41493eeebf31f67342af1da74043a37dd6b5cb
David Gardner <git@icmfp.com>
2016-05-06 14:46:20 +0100

    Split into a running and stopped class, so that both of these are options and they will clash correctly if both are requested.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,13 @@
-class puppetserver_wrapper::service {
+class puppetserver_wrapper::service::running {
   service { 'puppetserver':
     ensure => running,
     enable => true,
+  }
+}
+
+class puppetserver_wrapper::service::stopped {
+  service { 'puppetserver':
+    ensure => stopped,
+    enable => false,
   }
 }
